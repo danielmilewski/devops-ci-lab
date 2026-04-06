e473a29b06db0bf2c7d38404deff4401700c3915
danielmilewski <milewski.daniel@gmail.com>
2026-04-06 13:31:42 +0200

    Add hostname to app response

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import socket
+
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Version 2"
+def hello():
+    return f"Hello from DevOps CI/CD Lab! Hostname: {socket.gethostname()}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
